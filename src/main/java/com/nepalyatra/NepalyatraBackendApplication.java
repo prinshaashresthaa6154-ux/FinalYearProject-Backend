d4a7085e054520c32468d8a96d41b4bd67f07554
prinseyyy <prinshaashresthaa6154@gmail.com>
2026-03-18 22:11:48 +0545

    First Commit

---
+++ src/main/java/com/nepalyatra/NepalyatraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nepalyatra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NepalyatraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NepalyatraBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nepalyatra/NepalyatraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nepalyatra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NepalyatraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
